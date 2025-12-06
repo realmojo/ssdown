1bc75005344ce923e6cb6938c8620502fe9a56d2
realmojo <strikers1999@naver.com>
2025-12-07 03:07:59 +0900

    ef

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 import { i18n } from "./lib/i18n-config"
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname
   
   // Check if there is any supported locale in the pathname
